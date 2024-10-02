49a1b921ec57d96928ff1c06fd766a74f87252e1
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2024-10-02 13:44:40 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,6 @@
+def call(String projectName, String ImageTag){
+  withCredentials([usernamePassword(credentialsId:'dockerhub',passwordCredentials:'dockerHubPass',usernameCredentials:'dockerHubUser']){
+    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+  }
+  sh "docker push ${dockerHubUser}/${projectName}:${ImageTag}"
+}
