51f69f763d21b17ae51dae8d1a81b508950664d5
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2024-10-02 13:49:37 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,4 +1,4 @@
-def call(String projectName, String ImageTag){
+def call(Strng dockerHubUser,String projectName, String ImageTag){
   withCredentials([usernamePassword(credentialsId:'dockerhub',passwordVariable:'dockerHubPass',usernameVariable:'dockerHubUser')]){
     sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
   }
